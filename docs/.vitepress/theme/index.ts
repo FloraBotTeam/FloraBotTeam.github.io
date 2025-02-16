c68c0ed09c5c7101074102864c1071e922831811
YC酱luyancib <2058282593@qq.com>
2025-02-16 01:03:17 +0000

    Add EnhancedReadabilities plugin

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -9,15 +9,24 @@ import { NolebaseInlineLinkPreviewPlugin } from "@nolebase/vitepress-plugin-inli
 import '@nolebase/vitepress-plugin-inline-link-preview/client/style.css';
 import { NolebaseGitChangelogPlugin } from '@nolebase/vitepress-plugin-git-changelog/client'
 import '@nolebase/vitepress-plugin-git-changelog/client/style.css'
+import { NolebaseEnhancedReadabilitiesMenu, NolebaseEnhancedReadabilitiesScreenMenu } from "@nolebase/vitepress-plugin-enhanced-readabilities";
+import '@nolebase/vitepress-plugin-enhanced-readabilities/client/style.css'
 import DataPanel from "./components/DataPanel.vue";
 import Confetti from "./components/Confetti.vue";
 import Linkcard from "./components/Linkcard.vue"
 import ArticleMetadata from "./components/ArticleMetadata.vue"
+import { h } from "vue";
 
 
 
 export default {
     extends: DefaultTheme,
+    Layout: () => {
+      return h(DefaultTheme.Layout,null ,{
+        'nav-bar-content-after': () => h(NolebaseEnhancedReadabilitiesMenu),
+        'nav-screen-content-after': () => h(NolebaseEnhancedReadabilitiesScreenMenu)
+      })
+    },
     enhanceApp({ app, router }) {
       app.component("DataPanel", DataPanel);//注册全局组件
       app.component("Confetti", Confetti); //注册全局组件
