022ac9a3799d4fd31eaabb2aa1df074fe86cff7a
Wuchang <123864570+Wuchang325@users.noreply.github.com>
2025-02-16 17:02:40 +0800

    添加team变量

---
+++ docs/.vitepress/theme/data/team.ts
@@ -0,0 +1,28 @@
+const members = [
+    {
+        avatar: 'https://github.com/AEBC08.png',
+        name: 'AEBC08',
+        title: 'Creator/Maintainer',
+        links:[
+          { icon:'github',link:'https://github.com/AEBC08'}
+        ]
+      },
+      {
+        avatar: 'https://github.com/luyanci.png',
+        name: 'luyanci',
+        title: 'Document Maintainer',
+        links:[
+          { icon:'github',link:'https://github.com/luyanci'}
+        ]
+      },
+      {
+        avatar: 'https://github.com/wuchang325.png',
+        name: 'Wuchang325',
+        title: 'Document Maintainer',
+        links:[
+          { icon:'github',link:'https://github.com/wuchang325'}
+        ]
+      },
+]
+
+export default members;
